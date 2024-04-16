aec5d9d09f976c3d330eabaf4d3b514b91469881
Jan Lewandowski <iasiu@icloud.com>
2024-04-16 19:45:22 +0200

    Setup app flavors
    - add dev, tst and prod flavors
    
    - set icon for each flavors
    
    - set different app name for each flavor
    
    - add main_dev, main_tst and main_prod dart files
    
    - add main_common and AppConfiguration
    
    Related to #2

--- mobile/android/app/src/main/kotlin/com/example/moti/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/example/moti/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.moti
+package pl.moti
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ mobile/lib/main_common.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:moti/app.dart';
+import 'package:moti/app_configuration.dart';
+
+Future<void> mainCommon(AppConfiguration appConfiguration) async {
+  runApp(const MtApp());
+}
